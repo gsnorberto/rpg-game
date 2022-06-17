92e7ca51648fd54b5393a3b1fb2f5e03c75ba966
Gabriel Norberto <sn_gabriel@outlook.com>
2022-06-17 12:24:51 -0300

    Cenário adicionado

---
+++ src/App.styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #24282F;
+    min-height: 1000vh;
+    color: #FFFFFF;
+`;
+
+export const Map = styled.div`
+    width: 480px;
+    height: 480px;
+    background-image: url('/assets/map.png');
+    background-position: left top;
+    background-size: 100%;
+`
