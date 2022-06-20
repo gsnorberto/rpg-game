9990a3a0ab6fe4208dc740ea12a3bb66178195b3
Gabriel Norberto <sn_gabriel@outlook.com>
2022-06-20 10:50:25 -0300

    Criado personagem e hook de movimentação

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import * as C from './App.styles'
+import * as C from './App.styles';
+import { Character } from './components/Character';
+import { useCharacter } from './hooks/useCharacter'; // hook
 
 const App = () => {
+    const char = useCharacter();
+
     return (
         <C.Container>
             <C.Map>
-                
+                <Character x={char.x} y={char.y}/>
             </C.Map>
         </C.Container>
     );
